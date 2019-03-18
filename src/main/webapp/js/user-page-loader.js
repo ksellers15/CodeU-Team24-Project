ef0c10453778d53991e6c21a15271f9dee9f4476
hongbaoshila <ruby.hongbaoshi@163.com>
2019-03-18 18:47:37 -0400

    remove unused line

--- src/main/webapp/js/user-page-loader.js
+++ src/main/webapp/js/user-page-loader.js
@@ -49,7 +49,6 @@ function showMessageFormIfViewingSelf() {
 
 /** Fetches messages and add them to the page. */
 function fetchMessages() {
-  //const url = '/messages?user=' + parameterUsername;
   const parameterLanguage = urlParams.get('language');
   let url = '/messages?user=' + parameterUsername;
   if(parameterLanguage) {
