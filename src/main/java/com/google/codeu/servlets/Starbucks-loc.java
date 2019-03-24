32c4d616252f539ae72a148fef4821c74664f749
Chigozie Nna <Kv154chigo@gmail.com>
2019-03-24 05:18:08 -0400

    Starbucks Locations map

---
+++ src/main/java/com/google/codeu/servlets/Starbucks-loc.java
@@ -0,0 +1,58 @@
+package com.google.codeu.servlets;
+
+import java.io.IOException;
+import java.util.Scanner;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+
+/**
+ * Returns Starbucks data as a JSON array, e.g. [{"lat": 38.4404675, "lng": -122.7144313}]
+ */
+@WebServlet("/Starbucks-loc")
+public class StarBucksMapServlet extends HttpServlet {
+
+  JsonArray starBucksLocArray;
+
+ @Override
+ public void init() {
+  starBucksLocArray = new JsonArray();
+  Gson gson = new Gson();
+  Scanner scanner = new Scanner(getServletContext().getResourceAsStream("/WEB-INF/Starbucks-loc.csv"));
+  while(scanner.hasNextLine()) {
+   String line = scanner.nextLine();
+   String[] cells = line.split(",");
+
+   String state = cells[0];
+   String country = cells[1];
+   double lat = Double.parseDouble(cells[2]);
+   double lng = Double.parseDouble(cells[3]);
+
+   starBucksLocArray.add(gson.toJsonTree(new StarBucks(state, country, lat, lng)));
+  }
+  scanner.close();
+ }
+
+ @Override
+ public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+  response.setContentType("application/json");
+  response.getOutputStream().println(starBucksLocArray.toString());
+ }
+
+ private static class StarBucks{
+  String state;
+  String country;
+  double lat;
+  double lng;
+
+  private StarBucks(String state, String country, double lat, double lng) {
+   this.state = state;
+   this.country = country;
+   this.lat = lat;
+   this.lng = lng;
+  }
+ }
+}
