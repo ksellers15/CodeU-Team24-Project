a59e048644775eea8f11b1c7bffd5558684a2fa4
Kason Sellers <kasonsellers15@gmail.com>
2019-04-11 14:56:52 -0400

    Changed Home serlvet dependency from UserService to HttpSession.

--- src/main/java/com/google/codeu/servlets/HomeServlet.java
+++ src/main/java/com/google/codeu/servlets/HomeServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
 import com.google.appengine.api.users.UserService;
@@ -16,12 +17,16 @@ public class HomeServlet extends HttpServlet {
   @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 
-      UserService userService = UserServiceFactory.getUserService();
-      boolean isUserLoggedIn = userService.isUserLoggedIn();
+      HttpSession session = request.getSession();
+      boolean isUserLoggedIn = false;
+
+      if(session != null && session.getAttribute("logged_in") != null)
+        isUserLoggedIn = (boolean) session.getAttribute("logged_in");
+
       request.setAttribute("isUserLoggedIn", isUserLoggedIn);
 
       if(isUserLoggedIn)
-        request.setAttribute("username", userService.getCurrentUser().getEmail());
+        request.setAttribute("username", session.getAttribute("email"));
 
       request.getRequestDispatcher("WEB-INF/jsp/home.jsp").forward(request, response);
     }
